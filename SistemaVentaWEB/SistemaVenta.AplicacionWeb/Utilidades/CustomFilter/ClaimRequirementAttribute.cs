f13a375235f4e5513d3669c295105e5065214b0d
Cairo <rcairor09@gmail.com>
2023-11-28 00:05:56 -0600

    filtros de requerimientos para acceso a los menus

---
+++ SistemaVentaWEB/SistemaVenta.AplicacionWeb/Utilidades/CustomFilter/ClaimRequirementAttribute.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Utilidades.CustomFilter
+{
+    public class ClaimRequirementAttribute : TypeFilterAttribute
+    {
+        public ClaimRequirementAttribute(string controlador, string accion) : base(typeof(ClaimRequirementFilter))
+        {
+            Arguments = new object[] { controlador, accion };
+        }
+
+
+    }
+}
